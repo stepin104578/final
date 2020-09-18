71f253ef5468b8e30bd1f04a8dd1969af2501c3f
stepin104578 <69068778+stepin104578@users.noreply.github.com>
2020-09-18 21:03:54 +0530

    Update calculator_operations.h

--- inc/calculator_operations.h
+++ inc/calculator_operations.h
@@ -44,4 +44,13 @@ int multiply(int operand1, int operand2);
 */
 int divide(int operand1, int operand2);
 
+/**
+* finds the greater of two operands
+* @param[in] operand1 
+* @param[in] operand2 
+* @return integer value of the operand1 > operand2
+
+*/
+int greater(int operand1, int operand2);
+
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
